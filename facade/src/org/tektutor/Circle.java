726af0ec741668f4825f1965f27f4cb7c2ae69e6
yogeshvar <yogeshvar@icloud.com>
2019-06-20 14:54:06 +0530

    added facade pattern

---
+++ facade/src/org/tektutor/Circle.java
@@ -0,0 +1,10 @@
+package org.tektutor;
+
+public class Circle implements Shape {
+
+	@Override
+	public void draw() {
+		System.out.println("Draw Circle");
+	}
+
+}
---
+++ facade/src/org/tektutor/Rectangle.java
@@ -0,0 +1,10 @@
+package org.tektutor;
+
+public class Rectangle implements Shape {
+
+	@Override
+	public void draw() {
+		System.out.println("Draw Rectangle");
+	}
+	
+}
