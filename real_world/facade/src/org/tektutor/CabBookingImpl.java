f44c7038bd2f7b4ae3bbb3597c94396003492faa
lama-vishal <1990vsll@gmail.com>
2019-07-09 14:51:22 +0530

    Added 2 more methods in class CabBooking

--- real_world/facade/src/org/tektutor/CabBookingImpl.java
+++ real_world/facade/src/org/tektutor/CabBookingImpl.java
@@ -1,10 +1,22 @@
 package org.tektutor;
 
+import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
+
 public class CabBookingImpl implements CabBooking {
 
 	@Override
 	public void book() {
-		System.out.println("Cab Booking -> Booked");
+		System.out.println("Request for the cab booking is made.");
+		cabArrived();
 	}
 
+	private void cabArrived() {
+		System.out.println("Your cab is arrived");
+		welcomeMessage();
+	}
+	
+	private void welcomeMessage() {
+		System.out.println("Welcome and have a great journey!");
+	}
+	
 }
