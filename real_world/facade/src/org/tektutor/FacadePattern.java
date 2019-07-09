2e6c6e2103473a789e13e8f91f1ef6982aadc45d
Shashank Sharma <shanksharma258@gmail.com>
2019-07-09 15:58:49 +0530

    facade changes

--- real_world/facade/src/org/tektutor/FacadePattern.java
+++ real_world/facade/src/org/tektutor/FacadePattern.java
@@ -3,7 +3,7 @@ package org.tektutor;
 public class FacadePattern {
 	public static void main(String[] args) {
 
-	    TravelPackageFacade travelPackageFacade=new TravelPackageFacadeImpl();
+	    TravelPackageFacade travelPackageFacade = new TravelPackageFacadeImpl();//created the object
 	    travelPackageFacade.book();
 	  }
 }
