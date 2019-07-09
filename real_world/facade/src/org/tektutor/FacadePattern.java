9526dbb64faecb4a52126fcf3b31fc7e3c153617
kumuddhanvijay <kumuddhanvijay@gmail.com>
2019-07-09 15:15:30 +0530

    Neeraj

--- real_world/facade/src/org/tektutor/FacadePattern.java
+++ real_world/facade/src/org/tektutor/FacadePattern.java
@@ -4,6 +4,7 @@ public class FacadePattern {
 	public static void main(String[] args) {
 
 	    TravelPackageFacade travelPackageFacade=new TravelPackageFacadeImpl();
+	    
 	    travelPackageFacade.book();
 	  }
 }
